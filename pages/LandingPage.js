f762ae72dff87eef576a3a657035b28984b31e50
baehyunji <wns038200@naver.com>
2024-06-24 23:53:20 +0900

    [LandingPage] - LandingPage ui

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -2,7 +2,7 @@ import Button from "@/components/Button";
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 import styles from "@/styles/LandingPage.module.css";
-
+import Image from "next/image";
 function LandingPage() {
   //테스트 코드
   const handleClick = () => {
@@ -24,41 +24,76 @@ function LandingPage() {
         </div>
         <div className={styles.contents}>
           <div className={styles.saveContent}>
-            <h2>
-              <span>원하는 링크</span>를<br />
-              저장하세요
-            </h2>
-            <p>
-              나중에 읽고 싶은 글, 다시 보고 싶은 영상, <br />
-              사고 싶은 옷, 기억하고 싶은 모든 것을 <br /> 한 공간에 저장하세요.
-            </p>
+            <div>
+              <h2>
+                <span>원하는 링크</span>를<br />
+                저장하세요
+              </h2>
+              <p>
+                나중에 읽고 싶은 글, 다시 보고 싶은 영상, <br />
+                사고 싶은 옷, 기억하고 싶은 모든 것을 <br /> 한 공간에
+                저장하세요.
+              </p>
+            </div>
+            <Image
+              src="/asset/SaveContent.png"
+              alt={"저장이미지"}
+              width={550}
+              height={450}
+            />
           </div>
           <div className={styles.managementContent}>
-            <h2>
-              링크를 폴더로 <br />
-              <span>관리</span> 하세요
-            </h2>
-            <p>
-              나만의 폴더를 무제한으로 만들고 <br />
-              다양하게 활용할 수 있습니다.
-            </p>
+            <div>
+              <h2>
+                링크를 폴더로 <br />
+                <span>관리</span> 하세요
+              </h2>
+              <p>
+                나만의 폴더를 무제한으로 만들고 <br />
+                다양하게 활용할 수 있습니다.
+              </p>
+            </div>
+            <Image
+              src="/asset/Management.png"
+              alt={"관리이미지"}
+              width={550}
+              height={450}
+              className={styles.leftImages}
+            />
           </div>
           <div className={styles.shareContent}>
-            <h2>
-              저장한 링크를 <br />
-              <span>공유</span>해 보세요.
-            </h2>
-            <p>
-              여러 링크를 폴더에 담고 공유할 수 있습니다. <br />
-              가족, 친구, 동료들에게 쉽고 빠르게 링크를 <br />
-              공유해 보세요.
-            </p>
+            <div>
+              <h2>
+                저장한 링크를 <br />
+                <span>공유</span>해 보세요.
+              </h2>
+              <p>
+                여러 링크를 폴더에 담고 공유할 수 있습니다. <br />
+                가족, 친구, 동료들에게 쉽고 빠르게 링크를 <br />
+                공유해 보세요.
+              </p>
+            </div>
+            <Image
+              src="/asset/ShareContent.png"
+              alt={"공유이미지"}
+              width={550}
+              height={450}
+            />
           </div>
           <div className={styles.searchContent}>
-            <h2>
-              저장한 링크를 <span>검색</span>해 보세요
-            </h2>
-            <p>중요한 정보들을 검색으로 쉽게 찾아보세요.</p>
+            <div>
+              <h2>
+                저장한 링크를 <span>검색</span> <br />해 보세요
+              </h2>
+              <p>중요한 정보들을 검색으로 쉽게 찾아보세요.</p>
+            </div>
+            <Image
+              src="/asset/SearchContent.png"
+              alt={"검색이미지"}
+              width={550}
+              height={450}
+              className={styles.leftImages}
+            />
           </div>
         </div>
       </div>
