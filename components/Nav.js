61c2679e8fc2b7d3db0729ecc86d2ba624a7f06c
baehyunji <wns038200@naver.com>
2024-06-25 23:37:41 +0900

    [Nav] - Nav 컴포넌트 ui 작성

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,54 @@
+import Link from "next/link";
 import styles from "@/styles/Nav.module.css";
+import { useRouter } from "next/router";
+import Button from "./Button";
+import Image from "next/image";
 
 function Nav() {
-  return <></>;
+  /* ui 작성을 위해 임의로 작성한 코드
+   true, false 바꾸면 nav ui 달라짐*/
+  const isLoggIn = false;
+  const user = {
+    name: "홍길동",
+  };
+
+  const router = useRouter();
+
+  const handleLinkClick = () => {
+    router.push("/login");
+  };
+
+  return (
+    <div className={styles.nav}>
+      <div className={styles.logoWrapper}>
+        <Link href="/">
+          <Image
+            src="/asset/Linkbrary.png"
+            alt="로고이미지"
+            width={128}
+            height={24}
+          />
+        </Link>
+      </div>
+      <div className={styles.userSection}>
+        {isLoggIn ? (
+          <div className={styles.userInfo}>
+            <Image
+              src="/asset/account.png"
+              alt="유저이미지"
+              width={28}
+              height={28}
+            />
+            <span>{user?.name}</span>
+          </div>
+        ) : (
+          <Button type="LandingLogin" onClick={handleLinkClick}>
+            로그인
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
