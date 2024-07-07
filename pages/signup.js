d9b09d9a1fd3e3ef2b2a4157bb01416738a7fee8
YUKIJOO <koreala1@naver.com>
2024-07-07 18:59:01 +0900

    [LoginPage]-"회원가입, 로그인 최종 수정"

--- pages/signup.js
+++ pages/signup.js
@@ -4,48 +4,10 @@ import Logo from "../styles/Logo.module.css";
 import Label from "@/components/Label";
 import Input from "@/components/Input";
 import styles from "../styles/Login.module.css";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import global from "../styles/LoginBox.module.css";
-import axios from "axios";
+import instance from "@/lib/axios";
 import Button from "@/components/Button";
-
-const validateEmail = () => {
-  const email = "string";
-
-  if (!email) {
-    return "Email is required!";
-  }
-
-  if (!email.includes("@")) {
-    return "Email is invalid!";
-  }
-
-  return "";
-};
-
-const validatePassword = () => {
-  if (!password) {
-    return "Password is required!";
-  }
-
-  if (!password.length < 8) {
-    return "Password must be at least 8 characters!";
-  }
-
-  return "";
-};
-
-const validatename = () => {
-  if (!name) {
-    return "Password confirmation is required!";
-  }
-
-  if (password !== name) {
-    return "Passwords do not match!";
-  }
-
-  return "";
-};
 
 const Signup = () => {
   const [values, setValues] = useState({
@@ -53,92 +15,23 @@ const Signup = () => {
     password: "",
     name: "",
   });
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setname] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [errors, setErrors] = useState({
-    email: "",
-    password: "",
-    name: "",
-  });
 
-  const handleEmailInputChange = useCallback((e) => {
-    setValues(e.target.value);
+  function handleChange(e) {
+    const { name, value } = e.target;
 
-    const errorMessage = validateEmail(e.target.value);
-    setErrors({
-      ...errors,
-      email: errorMessage,
-    });
-  }, []);
+    setValues((preValues) => ({
+      ...preValues,
+      [name]: value,
+    }));
+  }
 
-  const handlePasswordInputChange = (e) => {
-    const _password = e.target.value;
-    setValues(_password);
-
-    const errorMessage = validatePassword(_password);
-    setErrors({
-      ...errors,
-      password: errorMessage,
-    });
-  };
-
-  const handlenameInputChange = (e) => {
-    const _name = e.target.value;
-    setValues(_name);
-
-    const errorMessage = validatename(password, _name);
-    setErrors({
-      ...errors,
-      name: errorMessage,
-    });
-  };
-
-  const handleRegisterFormSubmit = async (e) => {
+  async function handleSubmit(e) {
     e.preventDefault();
 
-    const emailErrorMessage = validateEmail(email);
-    if (emailErrorMessage) {
-      alert(emailErrorMessage);
-      return;
-    }
-
-    const passwordErrorMessage = validateEmail(password);
-    if (passwordErrorMessage) {
-      alert(passwordErrorMessage);
-      return;
-    }
-
-    const nameErrorMessage = validateEmail(name);
-    if (nameErrorMessage) {
-      alert(nameErrorMessage);
-      return;
-    }
-
-    try {
-      setIsSubmitting(true);
-
-      const response = await axios.post(
-        "https://linkbrary-api.vercel.app/6-5/auth/sign-up",
-        {
-          email,
-          password,
-          name,
-        }
-      );
-      if (response.status !== 200) {
-        throw new Error(response.data.message);
-      }
-    } catch (error) {
-      console.error(error);
-      alert("회원가입에 실패했습니다. 관리자에게 문의해주세요!");
-    } finally {
-      setIsSubmitting(false);
-    }
-
-    alert("Form submitted!");
-  };
+    const { name, email, password } = values;
+    await instance.post("/auth/sign-up", { email, password, name });
+    redirectToHomePage();
+  }
 
   return (
     <>
@@ -159,7 +52,7 @@ const Signup = () => {
           </div>
         </div>
 
-        <form className={styles.Form} onSubmit={handleRegisterFormSubmit}>
+        <form className={styles.Form} onSubmit={handleSubmit}>
           <Label className={styles.Label} htmlFor="email">
             이메일
           </Label>
@@ -168,11 +61,12 @@ const Signup = () => {
             className={styles.Input}
             name="email"
             type="email"
-            placeholder="이메일"
+            placeholder="example@email.com"
             value={values.email}
-            onChange={handleEmailInputChange}
+            onChange={handleChange}
+            required
           />
-          {errors.email && <p className="error-message">{errors.email}</p>}
+
           <Label className={styles.Label} htmlFor="password">
             비밀번호
           </Label>
@@ -181,30 +75,27 @@ const Signup = () => {
             className={styles.Input}
             name="password"
             type="password"
-            placeholder="비밀번호"
+            placeholder="특수문자 포함, 16자 이상"
             value={values.password}
-            onChange={handlePasswordInputChange}
+            onChange={handleChange}
           />
-          {errors.password && (
-            <p className="error-password">{errors.password}</p>
-          )}
+
           <Label className={styles.Label} htmlFor="password">
-            비밀번호확인
+            이름
           </Label>
           <Input
-            id="password"
+            id="name"
             className={styles.Input}
-            name="password"
-            type="password"
-            placeholder="비밀번호확인"
-            value={values.password}
-            onChange={handlenameInputChange}
+            name="name"
+            type="text"
+            placeholder="이름"
+            value={values.name}
+            onChange={handleChange}
           />
-          {errors.name && <p className="error-message">{errors.name}</p>}
-          <Button variant="Signup" type="submit" disabled={isSubmitting}>
+          <Button variant="Signup" type="submit">
             회원가입
           </Button>
-          <Indicator isLodaing={isSubmitting} />
+          <Indicator />
         </form>
       </div>
     </>
@@ -213,6 +104,11 @@ const Signup = () => {
 
 export default Signup;
 
-function Indicator({ isLodaing }) {
-  return <div>{isLodaing ? <p>Loaindg...</p> : <p>Loaded</p>}</div>;
+function redirectToHomePage() {
+  const homePageUrl = "/LoginPage";
+  window.location.href = homePageUrl;
+}
+
+function Indicator({ isLoaging }) {
+  return <div>{isLoaging ? <p>Loding...</p> : <p>Loaded</p>}</div>;
 }
